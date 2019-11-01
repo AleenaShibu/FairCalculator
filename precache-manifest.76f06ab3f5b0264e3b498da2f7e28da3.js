16e0c18d78ed94c22ea36b1da450783ee2874cfe
Aleena Shibu <aleenashibu94@gmail.com>
2019-11-01 17:28:56 +0530

    Updates

---
+++ precache-manifest.76f06ab3f5b0264e3b498da2f7e28da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e0d9e65e7f01cb70e3211f99353643",
+    "url": "/FairCalculator/index.html"
+  },
+  {
+    "revision": "4ec9543a098a93a79cec",
+    "url": "/FairCalculator/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fc956faea984e5fc331",
+    "url": "/FairCalculator/static/js/2.97cbd6f8.chunk.js"
+  },
+  {
+    "revision": "4ec9543a098a93a79cec",
+    "url": "/FairCalculator/static/js/main.12af632f.chunk.js"
+  },
+  {
+    "revision": "392f9a1c8538a38ac37b",
+    "url": "/FairCalculator/static/js/runtime-main.d5b54d1f.js"
+  }
+]);
