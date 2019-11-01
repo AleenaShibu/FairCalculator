94ebb72f5e3974905805ed0d392161deaf2af1a2
Aleena Shibu <aleenashibu94@gmail.com>
2019-11-01 17:08:51 +0530

    made changes stationDetails we can choose starting point and destination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import stationDetails from './stationDetails'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      stations:[],
+    }
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+    componentDidMount() {
+      const stationName = stationDetails.map(( stations) => 
+        {return stations} )
+    console.log(stationName)
+    this.setState(
+      { stations: stationName }
+       
+    )
+    
+  }
+  
+  render() {
+    let stations = this.state.stations
+    let optionsItems = stations.map((station) => <option key={station.stop_name}>{station.stop_name}</option>)
+  
 
-export default App;
+  
+     return (
+       <div>
+         <h1> Fare calculator</h1>
+         <form>
+         <label>Starting Point:</label>
+         <select>
+                {optionsItems}
+             </select>
+             <br/>
+             <label>Destination Point:</label>
+             <select>
+                {optionsItems}
+             </select>
+
+         </form>
+       </div>      
+            
+     )
+    }
+  }
+
+  
+  
+  export default App
